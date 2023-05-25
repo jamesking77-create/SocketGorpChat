f260052ece4d854a1ea3dae88994bc3bfde3aa6e
james king <jamesasuelimen77@gmail.com>
2023-05-25 23:35:43 +0100

    initial commit

---
+++ src/socketGroupChat/Server.java
@@ -0,0 +1,15 @@
+package socketGroupChat;
+
+import java.net.ServerSocket;
+
+public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer(){
+
+    }
+}
