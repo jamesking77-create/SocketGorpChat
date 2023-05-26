940812f920fa7cb42163e98bb99bb280d3d68af8
james king <jamesasuelimen77@gmail.com>
2023-05-26 15:31:26 +0100

    update

--- src/socketGroupChat/Server.java
+++ src/socketGroupChat/Server.java
@@ -1,11 +1,12 @@
 package socketGroupChat;
 
 import javax.imageio.IIOException;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    private ServerSocket serverSocket;
+    private final ServerSocket serverSocket;
 
 
 
@@ -15,32 +16,32 @@ public class Server {
     }
 
     public void startServer(){
-//        try{
-//            while (!serverSocket.isClosed()){
-//                Socket socket = serverSocket.accept();
-//                System.out.println("A client has connected");
-//                ClientHandler clientHandler = new ClientHandler(socket);
-//
-//                Thread thread = new Thread(clientHandler);
-//                thread.start();
-//            }
-//        }catch (IIOException e){
-//
-//        }
-//    }
-//    public  void closeServerSocket(){
-//        try{
-//            if (serverSocket != null){
-//                serverSocket.close();
-//            }
-//        }catch (IIOException e){
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static void main(String[] args) throws IIOException {
-//        ServerSocket serverSocket1 = new ServerSocket(1234);
-//        Server server = new Server(serverSocket1);
-//        server.startServer();
-//    }
-}}
+        try{
+            while (!serverSocket.isClosed()){
+                Socket socket = serverSocket.accept();
+                System.out.println("A client has connected");
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        }catch (IOException e){
+
+        }
+    }
+    public  void closeServerSocket(){
+        try{
+            if (serverSocket != null){
+                serverSocket.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket1 = new ServerSocket(1234);
+        Server server = new Server(serverSocket1);
+        server.startServer();
+    }
+}
